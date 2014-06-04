7a740fd864d6026e65078e831a8cf33005ecf299
Steffen Kolb <sapmme@gmail.com>
2014-06-04 10:40:28 +0200

    ADDED levelcreation lua files

--- base/initialize.lua
+++ base/initialize.lua
@@ -3,5 +3,6 @@ print("Initializing from lua...")
 
 Scripting:registerScript("gppp/gppp_initialize.lua")
 Scripting:registerScript("gppp/gppp_update.lua")
+Scripting:registerScript("gppp/gppp_levelcreation.lua")
 
 print("Finished initializing from lua.")
