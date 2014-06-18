08dc5e100c1db307db451afd5445351da679445f
KoalaAlex <s_alexander@hotmail.de>
2014-06-18 11:16:12 +0200

    Fusion Chen & ALex

--- scripts/gppp/space_surfer_initialize2.lua
+++ scripts/gppp/space_surfer_initialize2.lua
@@ -5,40 +5,34 @@ do -- Physics world
 	cinfo.worldSize = 2000.0
 	local world = PhysicsFactory:createWorld(cinfo)
 	PhysicsSystem:setWorld(world)
-
-	--PhysicsDebugView
-	PhysicsSystem:setDebugDrawingEnabled(true)
-
 end
+
+PhysicsSystem:setDebugDrawingEnabled(true)
 
 do -- debugCam
 	debugCam = GameObjectManager:createGameObject("debugCam")
 	debugCam.cc = debugCam:createCameraComponent()
-	-- debugCam.eye = Vec3(100.0, -100.0, 100.0)  -- camera position
-	debugCam.eye = Vec3(300, 0, 20)  -- camera position
-	debugCam.aim = Vec3(0.0, 0.0, 0.0) -- camera target
-	debugCam.rotSpeed = 20.0
-	debugCam.zoomSpeed = 50.0
-	debugCam.maxZoom = 1000.0
-	debugCam.minZoom = 10.0
-	debugCam.zoom = (debugCam.aim - debugCam.eye):length() -- initialer zoom, die länge des vektors: target-position
-	debugCam.cc:setPosition(debugCam.eye)
-	debugCam.cc:lookAt(debugCam.aim)
+	debugCam.cc:setPosition(Vec3(-600.0, 0.0, 0.0))
+	debugCam.cc:setViewDirection(Vec3(1.0, 0.0, 0.0))
+	debugCam.baseViewDir = Vec3(1.0, 0.0, 0.0)
+	debugCam.cc:setBaseViewDirection(debugCam.baseViewDir)
 end
 
-homeWorldSize = 50
+homeWorldSize = 200
 do -- homeWorld
 	homeWorld = {}
 	homeWorld.go = GameObjectManager:createGameObject("homeWorld")
 	homeWorld.pc = homeWorld.go:createPhysicsComponent()
 	local cinfo = RigidBodyCInfo()
 	cinfo.shape = PhysicsFactory:createSphere(homeWorldSize)
-	cinfo.motionType = MotionType.Keyframed
+	cinfo.motionType = MotionType.Dynamic
 	cinfo.position = Vec3(0, 0, 0)
-	cinfo.mass = 2
-	cinfo.friction = 0.4
-	cinfo.restitution = 0.8
-	cinfo.gravityFactor = 10
+	cinfo.mass = 9000
+	cinfo.friction = 1
+	cinfo.restitution = 1
+	cinfo.gravityFactor = 0
+
+
 	homeWorld.rb = homeWorld.pc:createRigidBody(cinfo)
 	homeWorld.sc = homeWorld.go:createScriptComponent()
 --	homeWorld.sc:setUpdateFunction(updateHomePlanet)
@@ -52,23 +46,28 @@ do	-- Character
 	character.go = GameObjectManager:createGameObject("character")
 	character.pc = character.go:createPhysicsComponent()
 	local cinfo = RigidBodyCInfo()
-	cinfo.shape = PhysicsFactory:createBox(Vec3(5,5,5))
+	cinfo.shape = PhysicsFactory:createBox(Vec3(20, 20, 20))
 	cinfo.motionType = MotionType.Character
-	cinfo.position = Vec3(0, 0, 70)
-	cinfo.mass = 2
-	cinfo.friction = 0.4
-	cinfo.restitution = 0.8
+	cinfo.restitution = 0
+	cinfo.friction = 0
+	cinfo.position = Vec3(0,0,500)
 	cinfo.gravityFactor = 10
-	cinfo.maxLinearVelocity = 150
+	cinfo.mass = 90
+	cinfo.maxLinearVelocity = 1000
+
+	cinfo.linearDamping = 1
+	cinfo.angularDamping = 1
 	character.rb = character.pc:createRigidBody(cinfo)
 	character.sc = character.go:createScriptComponent()
 	local renderComponent = character.go:createRenderComponent()
 	renderComponent:setPath("data/models/mario/mario.thModel")
-	--character.sc:setUpdateFunction(updateEnemy)
 	character.go:setComponentStates(ComponentState.Active)
+	-- collision event
+	--character.pc:getContactPointEvent():registerListener(collisionCharacter)
+	character.grounded = false
 end
 
-planetSize = 100
+planetSize = 50
 do	-- Planet
 	planet = {}
 	planet.go = GameObjectManager:createGameObject("planet")
@@ -76,17 +75,18 @@ do	-- Planet
 	local cinfo = RigidBodyCInfo()
 	cinfo.shape = PhysicsFactory:createSphere(planetSize)
 	cinfo.motionType = MotionType.Fixed
-	cinfo.position = Vec3(-40, 200, 40)
+	cinfo.position = Vec3(-40, 400, 40)
 	cinfo.mass = 2
 	cinfo.friction = 0.4
 	cinfo.restitution = 0.8
-	cinfo.gravityFactor = 10
+	cinfo.gravityFactor = 0
 	planet.rb = planet.pc:createRigidBody(cinfo)
 	planet.sc = planet.go:createScriptComponent()
 	planet.go:setComponentStates(ComponentState.Active)
+	planet.size = planetSize
 end
 
-planetTwoSize = 100
+planetSize = 100
 do	-- planetTwo
 	planetTwo = {}
 	planetTwo.go = GameObjectManager:createGameObject("planetTwo")
@@ -94,14 +94,15 @@ do	-- planetTwo
 	local cinfo = RigidBodyCInfo()
 	cinfo.shape = PhysicsFactory:createSphere(planetSize)
 	cinfo.motionType = MotionType.Fixed
-	cinfo.position = Vec3(-40, -200, 40)
+	cinfo.position = Vec3(-40, -400, 40)
 	cinfo.mass = 2
 	cinfo.friction = 0.4
 	cinfo.restitution = 0.8
-	cinfo.gravityFactor = 10
+	cinfo.gravityFactor = 0
 	planetTwo.rb = planetTwo.pc:createRigidBody(cinfo)
 	planetTwo.sc = planetTwo.go:createScriptComponent()
 	planetTwo.go:setComponentStates(ComponentState.Active)
+	planetTwo.size = planetSize
 end
 
 function defaultEnter(enterData)
--- scripts/gppp/space_surfer_update2.lua
+++ scripts/gppp/space_surfer_update2.lua
@@ -1,6 +1,5 @@
 print("running updates")
 
-
 function nextGUID()
 	local guid_string = tostring(guid)
 	guid = guid + 1
@@ -32,10 +31,10 @@ StateTransitions{
 	{ from = "__enter", to = "default" },
 }
 
-
 function updateHomePlanet()
 	local impulse = Vec3(0,0,0)
 	local acceleration = 100
+	local characterUpDirection = character.go:getUpDirection()
 	-- local planetPosition = homeWorld.go:getPosition()
 	
 	if(InputHandler:isPressed(Key.W)) then
@@ -50,6 +49,7 @@ function updateHomePlanet()
 	if(InputHandler:isPressed(Key.D)) then
 		impulse.y = -acceleration
 	end
+
 	
 	-- Kraft auf den Planeten
 	homeWorld.rb:setLinearVelocity(impulse)
@@ -58,73 +58,134 @@ function updateHomePlanet()
 	--DebugRenderer:printText(Vec2(-0.9, 0.65), "homeWorld: x " .. string.format("%.2f", planetPosition.x) .. ", y " .. string.format("%.2f", planetPosition.y) .. ", z " .. string.format("%.2f", planetPosition.z))
 end
 
+-- function updateCharacter(  )
+	-- -- body
+	-- DebugRenderer:printText(Vec2(-0.9, 0.7), "updateCharacter ")
+
+	-- local charPos = character.go:getWorldPosition()
+	-- local view = character.go:getViewDirection()
+	-- local impulse = Vec3(0,0,0)
+	-- local acceleration = 50
+	-- local rotationSpeed = 50
+	-- local rotation = Vec3(0,0,0)
+	-- local temp
+	-- local moveSpeed = 1200
+	-- -- Vector of gravitation
+	-- local gravPlanet = (homeWorld.go:getWorldPosition() - character.go:getWorldPosition()):mulScalar(15)
+	
+	-- -- Draw Direction Arrow
+	-- DebugRenderer:drawArrow(charPos, charPos + view:mulScalar(25.0))
+	
+	-- --local viewDirection = Vec3(0,0,0)
+
+	-- if(InputHandler:isPressed(Key.Up)) then
+
+		-- --impulse = (impulse + viewDirection):mulScalar(acceleration)
+		-- character.rb:applyLinearImpulse(view:mulScalar(moveSpeed))
+	-- end
+	-- if(InputHandler:isPressed(Key.Down)) then
+		-- --impulse = (impulse - viewDirection):mulScalar(acceleration)
+		-- character.rb:applyLinearImpulse(view:mulScalar(-moveSpeed))
+	-- end
+	-- if(InputHandler:isPressed(Key.Left)) then
+		-- rotation.z = rotationSpeed
+	-- end
+	-- if(InputHandler:isPressed(Key.Right)) then
+		-- rotation.z = -rotationSpeed
+	-- end
+	-- if(InputHandler:isPressed(Key.Space)) then
+		
+		-- homeWorld.rb:setLinearVelocity(view:mulScalar(acceleration))
+	-- end
+
+	-- impulse = impulse + gravPlanet
+
+	-- if(character.grounded) then
+		-- impulse = Vec3(0,0,0)
+	-- end
+	
+	-- character.rb:applyLinearImpulse(impulse)
+
+	-- character.rb:applyAngularImpulse(rotation)
+
+	-- if(InputHandler:isPressed(Key.Space) and character.grounded) then
+		-- character.rb:applyForce(0.5, Vec3(0,0,15000))
+	-- end
+	
+	
+
+	-- -- camera.cc:setPosition(Vec3(charPos.x, charPos.y - 500, charPos.z + 100));
+	-- -- camera.cc:lookAt(charPos)
+	
+	-- --handleScreenBorder(character.go)
+	-- DebugRenderer:printText(Vec2(-0.9, 0.75), "character: x " .. string.format("%.2f", charPos.x) .. ", y " .. string.format("%.2f", charPos.y) .. ", z " .. string.format("%.2f", charPos.z))
+	-- DebugRenderer:printText(Vec2(-0.9, 0.55), "characterView: x " .. string.format("%.2f", view.x) .. ", y " .. string.format("%.2f", view.y) .. ", z " .. string.format("%.2f", view.z))
+	-- DebugRenderer:printText(Vec2(-0.9, 0.45), "gravPlanet: x " .. string.format("%.2f", gravPlanet.x) .. ", y " .. string.format("%.2f", gravPlanet.y) .. ", z " .. string.format("%.2f", gravPlanet.z))
+-- end
+
 function updateCharacter(  )
 	-- body
-	DebugRenderer:printText(Vec2(-0.9, 0.7), "updateCharacter ")
+	--DebugRenderer:printText(Vec2(-0.9, 0.7), "updateCharacter ")
 
-	local charPos = character.go:getWorldPosition()
-	local view = character.go:getViewDirection()
 	local impulse = Vec3(0,0,0)
-	local acceleration = 50
-	local rotationSpeed = 50
+	local acceleration = 400
 	local rotation = Vec3(0,0,0)
-	local temp
-	local moveSpeed = 1200
-	-- Vector of gravitation
-	local gravPlanet = (homeWorld.go:getWorldPosition() - character.go:getWorldPosition()):mulScalar(15)
-	
-	-- Draw Direction Arrow
-	DebugRenderer:drawArrow(charPos, charPos + view:mulScalar(25.0))
-	
-	--local viewDirection = Vec3(0,0,0)
+
+
+
+ 	local view = character.go:getViewDirection()
+
+ 	local characterUpDirection = character.go:getUpDirection()
+ 	local quaternion = Quaternion(characterUpDirection, 0)
+
+	DebugRenderer:drawArrow(view, view:mulScalar(150) )
 
 	if(InputHandler:isPressed(Key.Up)) then
-
-		--impulse = (impulse + viewDirection):mulScalar(acceleration)
-		character.rb:applyLinearImpulse(view:mulScalar(moveSpeed))
+		impulse.y = acceleration * view.y
+		impulse.x = acceleration * view.x
+		impulse.z = acceleration * view.z
 	end
 	if(InputHandler:isPressed(Key.Down)) then
-		--impulse = (impulse - viewDirection):mulScalar(acceleration)
-		character.rb:applyLinearImpulse(view:mulScalar(-moveSpeed))
+		impulse.y = -acceleration * view.y
+		impulse.x = -acceleration * view.x
+		impulse.z = -acceleration * view.z
 	end
 	if(InputHandler:isPressed(Key.Left)) then
-		rotation.z = rotationSpeed
+		--rotation.z = -2
+		quaternion = Quaternion(characterUpDirection, 1)
+		--impulse.x = -acceleration
 	end
 	if(InputHandler:isPressed(Key.Right)) then
-		rotation.z = -rotationSpeed
+		--rotation.z = 2
+		--impulse.x = acceleration
+		quaternion = Quaternion(characterUpDirection, -1)
 	end
+	
 	if(InputHandler:isPressed(Key.Space)) then
-		
-		--homeWorld.rb:setLinearVelocity(view:mulScalar(acceleration))
-		homeWorld.rb:setLinearVelocity(gravPlanet:mulScalar(0.1))
+		--homeWorld.rb:setLinearVelocity(characterUpDirection:mulScalar(-acceleration * 0.2))
+		homeWorld.rb:applyLinearImpulse(characterUpDirection:mulScalar(-acceleration * 2000))
 	end
 
-	impulse = impulse + gravPlanet
+	impulse = impulse + (homeWorld.go:getWorldPosition() - character.go:getWorldPosition()):mulScalar(100)
 
 
 	character.rb:applyLinearImpulse(impulse)
 
-	character.rb:applyAngularImpulse(rotation)
+	character.go:setRotation(quaternion * character.go:getWorldRotation())
 
-	if(InputHandler:isPressed(Key.Space) and character.grounded) then
-		character.rb:applyForce(0.5, Vec3(0,0,15000))
-	end
+	--character.rb:setAngularVelocity(rotation)
 
-	-- camera.cc:setPosition(Vec3(charPos.x, charPos.y - 500, charPos.z + 100));
-	-- camera.cc:lookAt(charPos)
-	
-	--handleScreenBorder(character.go)
-	DebugRenderer:printText(Vec2(-0.9, 0.75), "character: x " .. string.format("%.2f", charPos.x) .. ", y " .. string.format("%.2f", charPos.y) .. ", z " .. string.format("%.2f", charPos.z))
-	DebugRenderer:printText(Vec2(-0.9, 0.55), "characterView: x " .. string.format("%.2f", view.x) .. ", y " .. string.format("%.2f", view.y) .. ", z " .. string.format("%.2f", view.z))
-	DebugRenderer:printText(Vec2(-0.9, 0.45), "gravPlanet: x " .. string.format("%.2f", gravPlanet.x) .. ", y " .. string.format("%.2f", gravPlanet.y) .. ", z " .. string.format("%.2f", gravPlanet.z))
+	charPos = character.go:getWorldPosition()
+
 end
+
 
 function updatePlanet(planet)
 	local impulse = Vec3(0,0,0)
 	local acceleration = 5
 	local PlanetPosition = planet.go:getWorldPosition()
 	local homeWorldPosition = homeWorld.go:getWorldPosition()
-	local gravPlanet = (PlanetPosition - homeWorldPosition):mulScalar(15)
+	local gravPlanet = (PlanetPosition - homeWorldPosition):mulScalar(1)
 	
 	-- Gravity to Planet
 	gravity = inGravityZone(planet)
@@ -135,6 +196,7 @@ function updatePlanet(planet)
 	-- Kraft auf den Planeten
 	local grav = homeWorld.rb:getLinearVelocity()
 	homeWorld.rb:setLinearVelocity(grav + impulse)
+	
 end
 
 -- Collision Detection in Gravity Sphere return a Value of the Power
@@ -145,6 +207,7 @@ function inGravityZone(planet)
 	-- x²+y²+z² 
 	local quadDistance = gravityHomePlanet.x * gravityHomePlanet.x + gravityHomePlanet.y * gravityHomePlanet.y + gravityHomePlanet.z * gravityHomePlanet.z
 	-- r1²+r2²+2*r1*r2  -------- RadiusPlanet = 100 RadiusHomePlanet = 50
+	local planetSize = planet.size * 1.5;
 	local radiusDistance = homeWorldSize*homeWorldSize + planetSize*planetSize + 2*homeWorldSize*planetSize
 	if(quadDistance < radiusDistance) then
 		return radiusDistance - quadDistance
