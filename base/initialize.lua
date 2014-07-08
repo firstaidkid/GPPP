e791cc7336e95e7a19667bc7d5bb9f547c969ce0
KoalaAlex <s_alexander@hotmail.de>
2014-07-09 00:56:53 +0200

    copy

--- base/initialize.lua
+++ base/initialize.lua
@@ -3,9 +3,11 @@ print("Initializing from lua...")
 
 --Scripting:registerScript("gppp/gppp_initialize.lua")
 --Scripting:registerScript("gppp/gppp_update.lua")
-Scripting:registerScript("initialize.lua")
-Scripting:registerScript("update.lua")
+
 --Scripting:registerScript("gppp/gppp_levelcreation.lua")
 
+
+Scripting:registerScript("initialize.lua")
+Scripting:registerScript("update.lua")
 
 print("Finished initializing from lua.")
